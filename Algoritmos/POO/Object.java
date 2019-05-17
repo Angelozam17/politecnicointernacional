35be283feb48ee5b7f5f87b42b83f25f9c2867b6
Angelo Zambrano <angelozam17@gmail.com>
2019-05-17 11:22:53 -0500

    Create Object.java

---
+++ Algoritmos/POO/Object.java
@@ -0,0 +1,16 @@
+public class Object{
+
+    public static void main(String[] args) {
+        
+        Persona Estudiante = new Persona();
+        Estudiante.get();
+        Estudiante.show();
+
+        Persona Profesor = new Persona();
+        Profesor.get();
+        Estudiante.show();
+
+
+    }
+
+}
