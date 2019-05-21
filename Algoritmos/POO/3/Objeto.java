11d4dbd30d478bc6b9d3b9c0762992bb2eb65246
Angelo Zambrano <angelozam17@gmail.com>
2019-05-21 12:29:15 -0500

    Create Objeto.java

---
+++ Algoritmos/POO/3/Objeto.java
@@ -0,0 +1,13 @@
+package horasextras;
+
+public class Objeto {
+   public static void main(String[] args){
+       Empleado empleado_1 = new Empleado();
+       empleado_1.personalData();
+       empleado_1.showData();
+       
+       Empleado empleado_2 = new Empleado();
+       empleado_2.personalData();
+       empleado_2.showData();
+   } 
+}
